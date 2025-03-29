bdbe12e88407ebaff314806d8f2f2bed74ee1389
TaqBostan <mehdi.rajaei87@gmail.com>
2025-03-29 14:13:46 +0330

    style

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { Director } from './Director';
 import { Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType } from './types';
 import Util from './util';
 import './helper';
+import './index.css';
 export { Director, Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType, Util }
