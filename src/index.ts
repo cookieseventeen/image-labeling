56d9b67ad076c5b1e450afcff747359ffbb9f487
TaqBostan <mehdi.rajaei87@gmail.com>
2025-03-31 09:08:11 +0330

    custom classes instead of SVG elements

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Director } from './Director';
 import { Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType } from './types';
 import Util from './util';
-import './helper';
+import { ImageEl, SVGSVGEl } from './svg-elems';
 import './index.css';
-export { Director, Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType, Util }
+export { Director, Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType, Util, ImageEl, SVGSVGEl }
