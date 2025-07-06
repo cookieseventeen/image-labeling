f86b94a1955aca723c957ee14eefc24563c21379
TaqBostan <mehdi.rajaei87@gmail.com>
2025-07-07 00:03:58 +0330

    extra exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Director } from './Director';
-import { Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType } from './types';
+import { Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Point, Color, ArrayXY, Shortcut, ActType } from './types';
 import Util from './util';
 import { ImageEl, SVGSVGEl } from './svg-elems';
 import './index.css';
-export { Director, Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Shortcut, ActType, Util, ImageEl, SVGSVGEl }
+export { Director, Shape, Polygon, Rectangle, Circle, Ellipse, Dot, Point, Color, ArrayXY, Shortcut, ActType, Util, ImageEl, SVGSVGEl }
